a7c18866521e8ee13088ed0a79e82c1c958581fa
Roni <veronicanartatez@Ronis-MacBook-Air.local>
2024-09-04 16:31:45 -0400

    tried to find code to get bot to reply to certain commands

--- pythonProject/quote_bot.py
+++ pythonProject/quote_bot.py
@@ -7,9 +7,10 @@ from dotenv import load_dotenv
 # Create a Discord client instance and set the command prefix
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
 
 # Set the confirmation message when the bot is ready
-@bot.event
+@client.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
@@ -28,6 +29,23 @@ async def list_command(ctx):
 async def functions(ctx):
     response = 'I am a simple Discord chatbot! I will reply to your command!'
     await ctx.send(response)
+
+message =
+
+@client.even
+
+
+#to get bot to respond
+@client.event
+async def on_message(message):
+    if message.content.startswith(“!”):
+        split_message = message.content.split()  # [“!!reply”, “messageid”]
+        if split_message[0] == “!reply”:
+            message_id = int(split_message[1])  # message.content is a str
+            reply_to = await message.channel.fetch_message(message_id)
+            await reply_to.reply(“Hello
+            World!”)
+
 
 # Retrieve token from the .env file
 load_dotenv()
